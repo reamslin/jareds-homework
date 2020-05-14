5e723c0297109d660e34a2dc40771e45f7709e0c
reamslin <reamslin@gmail.com>
2020-05-14 13:58:13 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+// for loops
+
+/**
+ * for (statement1 ; statement2 ; statement3) {
+ *      code-to-execute-each-iteration
+ * }
+ * 
+ * 
+ * statement1 : ran before the loop begins
+ * statement2 : condition that must be true to run loop again
+ * statement3 : what to do at the end of each iteration
+ */
+
+// Example:
+// This prints numbers 0 - 4
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+ // ASSIGNMENT
+ // this program should print the first 10 multiples of a factor
+
+ const factor = 5;
+
+ // fill in the blanks! You will need to use multiplication here.
+ // in javascript multiplication is *
+ // 6 * 4 returns 24  
+ for ( ; ; ) {
+    
+ }
+
+ // example: if factor = 5, this should print...
+ // 0
+ // 5
+ // 10
+ // 15
+ // 20
+ // 25
+ // 30
+ // 35
+ // 40
+ // 45
+ 
+
+ // make sure your program still works if you change the factor to be some other number
+ // if factor = 2, it should print :
+ // 0
+ // 2
+ // 4
+ // 6
+ // 8
+ // 10
+ // 12
+ // 14
+ // 16
+ // 18
